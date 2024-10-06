fde5ed7937dbc2911995145ac643cdc8c3dfdb06
violinist <vioolstudiorotterdam@gmail.com>
2024-10-06 22:12:47 +0200

    first commit

---
+++ src/main/java/nl/violinist/week2app/_main/Main.java
@@ -0,0 +1,24 @@
+package nl.violinist.week2app._main;
+
+import nl.violinist.week2app.util.Calculator;
+
+import java.util.Scanner;
+
+import static nl.violinist.week2app.service.ResultWriterService.*;
+
+public class Main {
+    public static void main (String [] args) {
+
+
+        Scanner cal = new Scanner(System.in);
+        int num1 = cal.nextInt();
+        int num2 = cal.nextInt();
+        int res1 = Calculator.sum(num1,num2);
+        int res2 = Calculator.difference(num1,num2);
+        int res3 = Calculator.multiplication(num1,num2);
+
+        print1(num1, num2, "sum",  res1);
+        print2(num1, num2, "difference", res2);
+        print3(num1, num2, "multiply", res3);
+    }
+}
---
+++ src/main/java/nl/violinist/week2app/util/Calculator.java
@@ -0,0 +1,18 @@
+package nl.violinist.week2app.util;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int difference(int a, int b) {
+        return a - b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+
+    }
+
+}
