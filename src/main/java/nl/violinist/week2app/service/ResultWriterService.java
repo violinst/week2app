669d873eab47e2f2e1495aace2ed8310f3a9fee3
violinist <vioolstudiorotterdam@gmail.com>
2024-10-08 22:04:21 +0200

    formatting commit: aligned

--- src/main/java/nl/violinist/week2app/service/ResultWriterService.java
+++ src/main/java/nl/violinist/week2app/service/ResultWriterService.java
@@ -2,7 +2,7 @@ package nl.violinist.week2app.service;
 
 public class ResultWriterService {
 
-    public static void print(String operation, int num1, int num2,  int result) {
+    public static void print(String operation, int num1, int num2, int result) {
         System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
     }
 }
